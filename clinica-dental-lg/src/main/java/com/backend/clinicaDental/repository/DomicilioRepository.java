30ca54cf4e91a117eaceb410bfaa5314cc240075
lizgiraldo <lizgiraldo15@gmail.com>
2024-06-15 15:56:22 -0500

    add repository Domicilio, Turno, Odontologo

---
+++ clinica-dental-lg/src/main/java/com/backend/clinicaDental/repository/DomicilioRepository.java
@@ -0,0 +1,4 @@
+package com.backend.clinicaDental.repository;
+
+public interface DomicilioRepository {
+}
--- clinica-dental-lg/src/main/java/com/backend/clinicaDental/repository/PacienteRepository.java
+++ clinica-dental-lg/src/main/java/com/backend/clinicaDental/repository/PacienteRepository.java
@@ -2,7 +2,9 @@ package com.backend.clinicaDental.repository;
 
 import com.backend.clinicaDental.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
     Paciente findByDni(int dni);
